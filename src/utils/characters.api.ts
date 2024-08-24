b95347b9a7c9961768b159d29a7b26455612c370
mariik <m.portnykh@fxdev.sk>
2024-08-24 10:33:43 +0200

    refactor: add comments for clearance

--- src/utils/characters.api.ts
+++ src/utils/characters.api.ts
@@ -1,5 +1,15 @@
 export const PAGE_LIMIT: number = 5;
 
+/**
+ * Returns a comma-separated string of the next 5 character IDs based on the page number
+ * to separate characters into pages.
+ *
+ * Calculates character IDs in sets of 5, depending on the page.
+ * For page 1, returns IDs 1-5; for page 2, returns IDs 6-10, and so on.
+ *
+ * @param {number} page - The current page number (starting from 1).
+ * @returns {string} - A string of 5 character IDs, separated by commas.
+ */
 const findNextCharacters = (page: number) => {
   // calculate ids for the 5 characters to devide them into pages
   const arr: number[] = Array.from(
